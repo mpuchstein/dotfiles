1f741b054df11257acfb86dcf730fb407ea47c05
mpuchstein <matthias@puchstein.bayern>
2025-04-02 20:46:30 +0200

    nvim theme settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,12 @@
 -- Initialize core settings first
 require("config.options")
+require("config.keymaps")
 
 -- Load plugin manager
 require("config.lazy")
 
+-- Plugin settings
+require("config.plugins.treesitter")
+
 -- Configure UI components last
+require("config.theme")
