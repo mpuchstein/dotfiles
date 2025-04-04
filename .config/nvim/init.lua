735f3baa37e477024846b96db11cafd6a6d0d122
mpuchstein <matthias@puchstein.bayern>
2025-04-04 16:45:31 +0200

    nvim settings continued

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,11 @@ require("config.lazy")
 
 -- Plugin settings
 require("config.plugins.treesitter")
+require("config.plugins.lsp")
+require("config.plugins.completion")
+require("config.plugins.telescope")
+require("config.plugins.explorer")
+require("config.plugins.whichkey")
 
 -- Configure UI components last
 require("config.theme")
