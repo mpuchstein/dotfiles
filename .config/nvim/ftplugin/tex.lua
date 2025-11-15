c7b4ad7b25770e06c499a2ae30ee66214e1ff2c3
Matthias Puchstein <matthias@puchstein.bayern>
2025-11-16 00:11:23 +0100

    fixed some nvim errors, added env vars and did some small changes to autoexec, disabled automatic profiles for hyprsunset

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,6 +1,6 @@
 -- Spell-check (tweak languages to your needs)
 vim.opt_local.spell = true
-vim.opt_local.spelllang = { "en_us", "de_de" }
+vim.opt_local.spelllang = { "en_gb", "de_de", "fr_fr" }
 
 -- Niceties for prose/math
 vim.opt_local.wrap = true
