fd6670e8b15b7f620c94bf0c41ad630c21f694b1
Matthias Puchstein <matthias@puchstein.bayern>
2025-11-17 09:56:39 +0100

    [nvim] fixed tex, [cs2] fixed sens, [zsh, profile] fixed pyenv

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,6 +1,6 @@
 -- Spell-check (tweak languages to your needs)
 vim.opt_local.spell = true
-vim.opt_local.spelllang = { "en_gb", "de_de", "fr_fr" }
+vim.opt_local.spelllang = { "en_gb", "de_de", "fr" }
 
 -- Niceties for prose/math
 vim.opt_local.wrap = true
